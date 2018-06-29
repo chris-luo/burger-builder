692f73df81167d0e1664c12889d41944c8a4b2e8
Chris Luo <chris.ying.luo@gmail.com>
2018-06-28 23:17:38 -0700

    Added empty burger text.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,10 +3,17 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const ingredients = Object.keys(props.ingredients)
+  let ingredients = Object.keys(props.ingredients)
     .map(igKey => [...Array(props.ingredients[igKey])].map((_, i) =>
       <BurgerIngredient key={igKey + i} type={igKey} />
-    ));
+    )).reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
+
+  if (ingredients.length === 0) {
+    ingredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
