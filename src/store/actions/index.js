71f2970fe15c53435ed41598216d68433ee19ca3
Chris Luo <chris.ying.luo@gmail.com>
2018-07-18 22:29:50 -0700

    added auth form and actions.

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,5 @@
 export { addIngredient, removeIngredient, fetchIngredients } from './burgerBuilder';
 
 export { purchaseInit, fetchOrders, purchaseBurger } from './orders';
+
+export { auth } from './auth';
