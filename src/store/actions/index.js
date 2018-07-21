fac565737829c9c4a727c239fb1a1b4168b35d8b
Chris Luo <chris.ying.luo@gmail.com>
2018-07-21 11:44:01 -0700

    persist token.

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,4 +2,4 @@ export { addIngredient, removeIngredient, fetchIngredients } from './burgerBuild
 
 export { purchaseInit, fetchOrders, purchaseBurger } from './orders';
 
-export { auth, logout, setAuthRedirectPath } from './auth';
+export { auth, logout, setAuthRedirectPath, authCheckState } from './auth';
